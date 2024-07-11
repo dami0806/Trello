30508d39d58995b79ad9fc63d314944f342ce310
DooDoo <160572286+Mua2@users.noreply.github.com>
2024-07-11 12:57:30 +0900

    feat[GLHL-39]:Column

--- src/main/java/com/sparta/trello/domain/column/entity/TrelloColumn.java
+++ src/main/java/com/sparta/trello/domain/column/entity/TrelloColumn.java
@@ -1,12 +1,10 @@
 package com.sparta.trello.domain.column.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -19,20 +17,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Column {
+public class TrelloColumn {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String title;
 
-	private Integer position;
+	private int position;
 
-	public Column updatePosition(Integer newPosition) {
-		return Column.builder()
-			.id(this.id)
-			.title(this.title)
-			.position(newPosition)
-			.build();
-	}
+	@ManyToOne
+	@JoinColumn(name = "board_id", nullable = false)
+	private Board board;
+
+
 }

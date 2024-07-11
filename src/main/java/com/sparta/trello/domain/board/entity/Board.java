d189596f2e787e3ab3266976a7a53071ff8cdcab
hyunmang2 <wowow19811@gmail.com>
2024-07-11 12:34:33 +0900

    feat: [GLHL-48] Board, BoardInvitation  Entity 생성

---
+++ src/main/java/com/sparta/trello/domain/board/entity/Board.java
@@ -0,0 +1,36 @@
+package com.sparta.trello.domain.board.entity;
+
+import com.sparta.trello.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "board")
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long boardId;
+
+    @Column(name = "boardname", nullable = false)
+    private String boardName;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    private BoardStatus boardStatus;
+
+    // Column Entity 생성 후 주석 해제
+//    @OneToMany(mappedBy = "column")
+//    private List<Column> columnList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "boardInvitationId")
+    private List<BoardInvitation> boardInvitationList = new ArrayList<>();
+
+}

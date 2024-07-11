a424ba7cca581c8bd00277f692e28b970242fc4f
DooDoo <160572286+Mua2@users.noreply.github.com>
2024-07-11 13:35:38 +0900

    feat[GLHL-39]:Column

--- src/main/java/com/sparta/trello/domain/column/entity/TrelloColumn.java
+++ src/main/java/com/sparta/trello/domain/column/entity/TrelloColumn.java
@@ -1,11 +1,18 @@
 package com.sparta.trello.domain.column.entity;
 
+import java.util.List;
+
+import com.sparta.trello.domain.card.entity.Card;
+import com.sparta.trello.domain.common.entity.BaseEntity;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,17 +24,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class TrelloColumn {
+public class TrelloColumn extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String title;
 
-	private int position;
+	private Integer position;
 
 	// @ManyToOne
 	// @JoinColumn(name = "board_id")
 	// private Board board;
+
+	// card와 매핑
+	@OneToMany(mappedBy = "trelloColumn", cascade = CascadeType.ALL)
+	private List<Card> cards;
 
 }

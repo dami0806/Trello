77dfc8b4290a3bc0b8c1e7cba9762f69d28e647f
hyunmang2 <wowow19811@gmail.com>
2024-07-12 20:05:02 +0900

    fix: [GLHL-48] 로그인 한 사용자만 보드 생성 할 수 있게 수정

--- src/main/java/com/sparta/trello/domain/board/controller/BoardController.java
+++ src/main/java/com/sparta/trello/domain/board/controller/BoardController.java
@@ -1,11 +1,14 @@
 package com.sparta.trello.domain.board.controller;
 
+import com.sparta.trello.domain.auth.service.AuthServiceImpl;
 import com.sparta.trello.domain.board.dto.request.BoardRequest;
 import com.sparta.trello.domain.board.dto.response.BoardResponse;
 import com.sparta.trello.domain.board.service.BoardService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,9 +18,10 @@ public class BoardController {
     private final BoardService boardService;
 
     @PostMapping("/boards")
-    public ResponseEntity<BoardResponse> createBoard(@RequestBody BoardRequest boardRequest) {
+    public ResponseEntity<BoardResponse> createBoard(@RequestBody BoardRequest boardRequest,
+                                                     @AuthenticationPrincipal AuthServiceImpl authServiceImpl) {
 
-        BoardResponse response = boardService.createBoard(boardRequest);
+        BoardResponse response = boardService.createBoard(boardRequest, authServiceImpl.getUser());
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 

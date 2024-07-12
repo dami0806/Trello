d5d8d2cf1e30be73238ff7210dd29b1369c8109f
hyunmang2 <wowow19811@gmail.com>
2024-07-12 20:07:44 +0900

    fix: [GLHL-48] User 매핑

--- src/main/java/com/sparta/trello/domain/board/entity/Board.java
+++ src/main/java/com/sparta/trello/domain/board/entity/Board.java
@@ -1,6 +1,5 @@
 package com.sparta.trello.domain.board.entity;
 
-import com.sparta.trello.domain.column.entity.TrelloColumn;
 import com.sparta.trello.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -28,16 +27,21 @@ public class Board {
     @Enumerated(EnumType.STRING)
     private BoardStatus boardStatus;
 
-    @OneToMany(mappedBy = "board")
-    private List<TrelloColumn> columnList = new ArrayList<>();
+//    @OneToMany(mappedBy = "board")
+//    private List<TrelloColumn> columnList = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @OneToMany(mappedBy = "boardInvitationId")
     private List<BoardInvitation> boardInvitationList = new ArrayList<>();
 
-    public Board(String boardName, String description, BoardStatus boardStatus) {
+    public Board(String boardName, String description, BoardStatus boardStatus, User user) {
         this.boardName = boardName;
         this.description = description;
         this.boardStatus = boardStatus;
+        this.user = user;
     }
 
     public void update(String boardName, String description) {

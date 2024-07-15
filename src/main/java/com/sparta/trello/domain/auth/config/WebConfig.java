cf0cb1e5fc6f800001954e85f58db2a08aa70b92
DooDoo <160572286+Mua2@users.noreply.github.com>
2024-07-15 14:40:39 +0900

    프론트 연결

---
+++ src/main/java/com/sparta/trello/domain/auth/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.sparta.trello.domain.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:3001") // 프론트엔드 주소
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+			.allowedHeaders("*")
+			.allowCredentials(true);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3001");
+			}
+		};
+	}
+}

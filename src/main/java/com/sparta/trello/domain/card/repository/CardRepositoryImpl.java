69a37db793503f650c679ec4801fd9da6c3826ea
dami0806 <dami0804@naver.com>
2024-10-19 12:19:48 +0900

    refactor: N+1문제 해결
    fetch join comment with user

--- src/main/java/com/sparta/trello/domain/card/repository/CardRepositoryImpl.java
+++ src/main/java/com/sparta/trello/domain/card/repository/CardRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sparta.trello.domain.comment.entity.Comment;
 import com.sparta.trello.domain.comment.entity.CommentStatus;
 import com.sparta.trello.domain.comment.entity.QComment;
+import com.sparta.trello.domain.user.entity.QUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -19,8 +20,10 @@ public class CardRepositoryImpl implements CardRepositoryCustom {
     @Override
     public Page<Comment> findCommentsByCardId(Long cardId, Pageable pageable) {
         QComment qComment = QComment.comment;
+        QUser qUser = QUser.user;
 
         List<Comment> comments = queryFactory.selectFrom(qComment)
+                .leftJoin(qComment.user, qUser).fetchJoin()  // 댓글과 작성자를 함께 조회
                 .where(qComment.card.id.eq(cardId).and(qComment.status.eq(CommentStatus.ACTIVE)))
                 .orderBy(qComment.createAt.desc())
                 .offset(pageable.getOffset())
